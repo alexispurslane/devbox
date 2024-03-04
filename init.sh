d427b54e0133c71c18ed3c82d24163b126ac728f
Alexis Purslane <alexispurslane@pm.me>
2024-03-04 13:17:10 -0500

    removed even more

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 set -o xtrace
 
-sudo pip install --break-system-packages tensorflow[and-cuda] spacy[cuda-12x] pyright
+pip install --break-system-packages tensorflow[and-cuda] spacy[cuda-12x] pyright
 python3 -m spacy download en_core_web_trf
 
 rustup toolchain install stable
@@ -9,21 +9,13 @@ rustup component add rust-analyzer
 opam init
 opam install ocamlbuild ocaml-lsp-server odoc merlin ocamlformat utop 
 
-sudo npm install -g prettier eslint stylelint xmllint js-beautify
-
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+npm install -g prettier eslint stylelint xmllint js-beautify
 
 git clone https://github.com/realnc/frobtads.git ~/frobtads
 cd ~/frobtads
 mkdir build
 cd build
-cmake .. 1>/dev/null
-sudo cmake --build . --target install 1>/dev/null
+cmake ..
+cmake --build . --target install
 cd ../..
 rm -rf frobtads
-
-mkdir -p ~/TADS/Extensions/
-git clone https://github.com/EricEve/adv3lite ~/TADS/Extensions/adv3lite/
-
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
