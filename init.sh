ecb898faf874e2026f55fbc9c7a007e0d7799063
Alexis Purslane <alexispurslane@pm.me>
2024-05-15 15:26:27 -0400

    tree sitter

--- init.sh
+++ init.sh
@@ -4,8 +4,6 @@ npm install -g prettier eslint stylelint xmllint js-beautify
 
 pip install tensorflow[and-cuda] spacy[cuda12x] pyright pyflakes black
 
-env RUSTUP_HOME=/usr/share/rustup CARGO_HOME=/usr/share/cargo rustup-init --default-toolchain stable --profile default --no-modify-path -y -c rust-analyzer
-
 git clone https://github.com/realnc/frobtads.git ~/frobtads
 cd ~/frobtads
 mkdir build
@@ -16,3 +14,10 @@ cd ../..
 rm -rf frobtads
 
 gem install bundler jekyll
+
+git clone https://github.com/casouri/tree-sitter-module
+cd tree-sitter-module
+./batch.sh
+cp dist/* /usr/local/lib/
+cd ..
+rm -rf tree-sitter-module
