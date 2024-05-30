849c1c651bd67a0cec8da4a6bc40928f1d7ccb3b
Alexis Purslane <alexispurslane@pm.me>
2024-05-29 22:32:56 -0400

    CI

--- Containerfile.cpp
+++ Containerfile.cpp
@@ -5,15 +5,10 @@ LABEL com.github.containers.toolbox="true" \
       summary="Fedora Rawhide container for Rust development" \
       maintainer="alexispurslane@pm.me"
 
-RUN export RUSTUP_HOME=/usr/local/rustup && \
-    export CARGO_HOME=/usr/local/rustup && \
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path && \
-    rustup component add rust-analyzer -y
-
 RUN dnf upgrade -y
 RUN dnf group install -y "C Development Tools and Libraries" "Development Tools"
 
-COPY extra-packages /
-RUN grep -v '^#' /extra-packages | xargs dnf install -y
-RUN rm /extra-packages
+COPY extra-packages.cpp /
+RUN grep -v '^#' /extra-packages.cpp | xargs dnf install -y
+RUN rm /extra-packages.cpp
 RUN dnf clean all
